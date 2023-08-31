b908e62bc2a43ae887f85813fae636984e0cb0ce
ShahriarMostafiz <shahriarmostafiz19@gmail.com>
2023-08-31 22:47:26 +0600

    cards updated

--- app.js
+++ app.js
@@ -22,31 +22,48 @@ const handleCards = async (data) => {
   const output = await response.json();
   //   console.log(output);
   let result = output?.data;
-
-  if (result.length === 0) {
-    result = ["No Data Availble"];
-  }
-
-  //   console.log(result);
   const cardContainer = document.getElementById("card-container");
   cardContainer.textContent = "";
-  result.forEach((card) => {
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("flex", "justify-center");
-    if (card?.others?.posted_date) {
-      const step0 = card.others.posted_date / (3600 * 24);
-      const day = parseInt(step0);
-      const hour = parseInt(card.others.posted_date / 3600);
-      const step1 = card.others.posted_date / 3600 - hour;
-      const minute = parseInt(step1 * 60);
-      const time = ` ${hour} h ${minute} m ago `;
-      //   const currentTime = new Date();
-      //   const currentHour = currentTime.getHours();
-      //   const currentMinutes = currentTime.getMinutes();
+  if (result.length === 0) {
+    const undefinedDiv = document.createElement("div");
+    undefinedDiv.classList.add(
+      "flex",
+      "lg:col-span-4",
+      "md:col-span-2",
+      "justify-center"
+    );
+    // .classList.add("w-full", "block", "border", "border-cyan-950 ");
+    undefinedDiv.innerHTML = `<div class="w-full     h-screen flex flex-col gap-4 justify-center items-center ">
+    <img src="./Icon.png" alt="" />
+    <p class= "text-3xl font-medium">No Data Available</p></div>`;
 
-      card.others.posted_date = time;
-    }
-    cardDiv.innerHTML = `
+    cardContainer.appendChild(undefinedDiv);
+  } else {
+    result.forEach((card) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("flex", "justify-center");
+      if (card?.others?.posted_date) {
+        const step0 = card.others.posted_date / (3600 * 24);
+        const day = parseInt(step0);
+        const hour = parseInt(card.others.posted_date / 3600);
+        const step1 = card.others.posted_date / 3600 - hour;
+        const minute = parseInt(step1 * 60);
+        const time = ` ${hour} h ${minute} m ago `;
+        //   const currentTime = new Date();
+        //   const currentHour = currentTime.getHours();
+        //   const currentMinutes = currentTime.getMinutes();
+
+        card.others.posted_date = time;
+      }
+
+      const url = "./fi_10629607.svg";
+
+      if (card?.authors[0]?.verified) {
+        card.authors[0].verified = url;
+      } else {
+        card.authors[0].verified = "";
+      }
+      cardDiv.innerHTML = `
     <div class=" card  w-96 h-[450px] bg-base-100 shadow-xl">
       <div class="relative h-48 border-2 ">
    <div class= " w-full h-full">
@@ -58,15 +75,29 @@ const handleCards = async (data) => {
    </div>;
 
   <div class="card-body">
-    <h2 class="card-title">${card.title}</h2>
+    <div class="flex gap-5 items-center">
+        <div>
+    <img class="w-[50px] rounded-full" src=${card?.authors[0]?.profile_picture} alt="not found">
+      </div>
+      <div class="w-full ">
+        <h1 class="text-2xl font-medium">${card.title}</h1>
+        <div class="flex gap-2 items-center">
+          <p class="text-gray-400  max-w-fit text-lg">${card?.authors[0]?.profile_name}</p>
+          <img src = '${card?.authors[0]?.verified}' >
+        </div>
+      </div>
+    </div>
 
-      <button class="btn btn-primary">Buy Now</button>
+     
     </div>
   </div>
 </div>
     `;
-    cardContainer.appendChild(cardDiv);
-  });
+      cardContainer.appendChild(cardDiv);
+    });
+  }
+
+  //   console.log(result);
 };
 loadCategory();
 handleCards("1000");
